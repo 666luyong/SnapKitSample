1eabf75d2b9b77cf0647a007de75abcb6eedfd2f
chenkai <kaidocumentgroup@gmail.com>
2016-07-05 15:34:30 +0800

    add snapkit framework

---
+++ SnapkitSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SnapkitSample
+//
+//  Created by chenkai on 16/7/3.
+//  Copyright © 2016年 chenkai. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
